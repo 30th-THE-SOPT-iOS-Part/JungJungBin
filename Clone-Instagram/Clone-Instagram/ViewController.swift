9956695fb6e7465c8eb4d2a56b6bec62ef4d3400
dev-madilyn <lollin0@naver.com>
2022-04-07 23:55:57 +0900

    Create: Clone-Instagram project

---
+++ Clone-Instagram/Clone-Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clone-Instagram
+//
+//  Created by madilyn on 2022/04/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
