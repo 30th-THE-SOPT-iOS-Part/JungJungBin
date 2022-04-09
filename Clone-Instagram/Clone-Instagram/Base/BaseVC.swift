fd9fb4bc45c0b312beada0b81be2db6e33b46091
dev-madilyn <lollin0@naver.com>
2022-04-09 18:25:19 +0900

    Add: BaseVC, SignInVC

---
+++ Clone-Instagram/Clone-Instagram/Base/BaseVC.swift
@@ -0,0 +1,15 @@
+//
+//  BaseVC.swift
+//  Clone-Instagram
+//
+//  Created by madilyn on 2022/04/09.
+//
+
+import UIKit
+
+class BaseVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
---
+++ Clone-Instagram/Clone-Instagram/SignInVC.swift
@@ -0,0 +1,18 @@
+//
+//  SignInVC.swift
+//  Clone-Instagram
+//
+//  Created by madilyn on 2022/04/08.
+//
+
+import UIKit
+
+class SignInVC: BaseVC {
+    
+    
+    // MARK: LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
