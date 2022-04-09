d6e61699bde6a32e70b83f60ed235edf74ff00dd
dev-madilyn <lollin0@naver.com>
2022-04-10 01:46:38 +0900

    Add: SignUpVC

---
+++ Clone-Instagram/Clone-Instagram/Scene/SignUpVC.swift
@@ -0,0 +1,16 @@
+//
+//  SignUpVC.swift
+//  Clone-Instagram
+//
+//  Created by madilyn on 2022/04/10.
+//
+
+import UIKit
+
+class SignUpVC: BaseVC {
+    
+    // MARK: LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
