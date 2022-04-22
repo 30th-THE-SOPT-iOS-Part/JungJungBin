37eada5baacabcd39710b5cc2656972405ff1275
dev-madilyn <lollin0@naver.com>
2022-04-22 22:36:05 +0900

    Chore: set VC's Background color

--- Clone-Instagram/Clone-Instagram/Scene/InstagramTabBar/Mypage/MypageVC.swift
+++ Clone-Instagram/Clone-Instagram/Scene/InstagramTabBar/Mypage/MypageVC.swift
@@ -7,23 +7,14 @@
 
 import UIKit
 
-class MypageVC: UIViewController {
+class MypageVC: BaseVC {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configureUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func configureUI() {
+        view.backgroundColor = .systemBlue
     }
-    */
-
 }
