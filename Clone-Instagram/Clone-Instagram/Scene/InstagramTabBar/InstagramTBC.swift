44e7343c3b08033fb986bdd38f2608dd0133f6d2
dev-madilyn <lollin0@naver.com>
2022-04-22 21:58:49 +0900

    Create: TabBarController, 각 ViewController 생성

---
+++ Clone-Instagram/Clone-Instagram/Scene/InstagramTabBar/InstagramTBC.swift
@@ -0,0 +1,16 @@
+//
+//  InstagramTBC.swift
+//  Clone-Instagram
+//
+//  Created by madilyn on 2022/04/22.
+//
+
+import UIKit
+
+final class InstagramTBC: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
