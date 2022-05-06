07b7bb5e770e5c6eea849918702cd981b701e374
dev-madilyn <lollin0@naver.com>
2022-05-06 23:22:10 +0900

    Chore: NaviView 구현

--- Clone-Instagram/Clone-Instagram/Scene/InstagramTabBar/Home/HomeVC.swift
+++ Clone-Instagram/Clone-Instagram/Scene/InstagramTabBar/Home/HomeVC.swift
@@ -8,14 +8,23 @@
 import UIKit
 
 class HomeVC: BaseVC {
-
+    
+    // MARK: Properties
+    private let naviView = HomeNaviView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
     }
     
     private func configureUI() {
-        view.backgroundColor = .systemRed
+        view.addSubviews([naviView])
+        
+        naviView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            $0.leading.equalTo(view.safeAreaLayoutGuide.snp.leading)
+            $0.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing)
+            $0.height.equalTo(44)
+        }
     }
 }
-
