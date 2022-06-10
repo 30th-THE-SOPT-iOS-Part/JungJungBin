82830cfe8f2c6284fd1dd6cdd591a5bc213b87df
dev-madilyn <lollin0@naver.com>
2022-06-10 19:08:06 +0900

    Add: SignInDataModel, SignUpDataModel, SignBodyModel 작성

---
+++ Clone-Instagram/Clone-Instagram/Scene/SignUp/SignBodyModel.swift
@@ -0,0 +1,14 @@
+//
+//  SignBodyModel.swift
+//  Clone-Instagram
+//
+//  Created by madilyn on 2022/06/10.
+//
+
+import Foundation
+
+struct SignBodyModel: Codable {
+    let name: String
+    let email: String
+    let password: String
+}
