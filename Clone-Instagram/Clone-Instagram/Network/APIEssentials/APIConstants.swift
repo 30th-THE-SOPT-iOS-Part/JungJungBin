2b4c36c1a1ff868129473246ae07bbd333dad054
dev-madilyn <lollin0@naver.com>
2022-06-10 18:08:39 +0900

    Chore:APIConstants, GenericResponse 세팅

--- Clone-Instagram/Clone-Instagram/Network/APIEssentials/APIConstants.swift
+++ Clone-Instagram/Clone-Instagram/Network/APIEssentials/APIConstants.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+import Alamofire
+
+struct APIConstants {
+    // MARK: Base URL
+    static let baseURL = "http://13.124.62.236"
+    
+    // MARK: Feature URL
+    static let postSignUp = baseURL + "/auth/signup"
+    static let postSignIn = baseURL + "/auth/signin"
+}
